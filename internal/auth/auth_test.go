42ea208b302a6a0a0f5e02c7359ebf9525772e9b
Emma Ahmad <emmaahmadsir@gmail.com>
2024-06-15 14:12:06 +0800

    unit test that does nothing

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	// var r *http.Request
+	// r.Header.Set("Authorization", "domain.tld")
+	// _, err := GetAPIKey(r.Header)
+	// if err != nil {
+	// 	t.Error("GetAPIKey failed")
+	// 	return
+	// }
+	t.Log("GetAPIKey passed")
+
+}
